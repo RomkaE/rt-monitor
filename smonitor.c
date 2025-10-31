255cdb2ea1d89c02243ec26ac6ab9512de63a0d4
Roman Egoshin <egoshin.roman@gmail.com>
2025-10-31 23:02:18 +0100

    Code format

--- smonitor.c
+++ smonitor.c
@@ -41,6 +41,9 @@
   #error SYS_MON_LINE_BUFF_SIZE cannot be less than 32
 #endif
 
+#define PREFIX_SIZE     ( sizeof(CLEAREOL) - 1 )
+#define BUF_SIZE        ( PREFIX_SIZE + SYS_MON_LINE_BUFF_SIZE)
+
 static TaskStatus_t s_Tasks[SMON_TASKS_MAX_COUNT];
 
 static const char *s_TaskState[] = {
@@ -56,17 +59,22 @@ static StackType_t uxSMonTaskStack[SMON_TASK_STACK_DEPTH];
 static SemaphoreHandle_t s_SemXmitHandle;
 static StaticSemaphore_t s_SemXmit;
 
-void smon_printf(const char* format_msg, ...)
+void rtmon_xmitCmpltCallback(void)
 {
-  static char buf[3 + SYS_MON_LINE_BUFF_SIZE] = CLEAREOL;
-  const size_t size = sizeof(buf);
-  //const char *line = &buf[4];
-  int len = 3;
+  BaseType_t res = xSemaphoreGive(s_SemXmitHandle);
+  assert(res == pdTRUE);
+}
+
+static void print(const char* format_msg, ...)
+{
+  static char s_Buf[BUF_SIZE] = CLEAREOL;
+  char *const line = &s_Buf[PREFIX_SIZE];
+  const size_t size = SYS_MON_LINE_BUFF_SIZE;
 
   // Message:
   va_list p_args;
   va_start(p_args, format_msg);
-  len += vsnprintf(&buf[3], size, format_msg, p_args);
+  int len = vsnprintf(line, size, format_msg, p_args);
   va_end(p_args);
 
   // Check and write:
@@ -75,24 +83,24 @@ void smon_printf(const char* format_msg, ...)
     // Check and fix to max length:
     if (len + 2 > size)
     {
-      memcpy(&buf[size - 5], "...\r\n", 5);
+      memcpy(&line[size - 5], "...\r\n", 5);
       len = size;
     }
     else
     {
-     buf[len++] = '\r';
-     buf[len++] = '\n';
+      line[len++] = '\r';
+      line[len++] = '\n';
     }
 
     // Send:
-    rtmon_xmitBuf(buf, len);
+    rtmon_xmitBuf(s_Buf, PREFIX_SIZE + len);
 
     // Wait:
     xSemaphoreTake(s_SemXmitHandle, pdMS_TO_TICKS(100));  // TODO - check result
   }
 }
 
-uint16_t calc_load(configRUN_TIME_COUNTER_TYPE _busy, configRUN_TIME_COUNTER_TYPE _elapsed)
+static uint16_t calc_load(configRUN_TIME_COUNTER_TYPE _busy, configRUN_TIME_COUNTER_TYPE _elapsed)
 {
   uint32_t load = (uint32_t)_busy * SCALE * 100;
   load = load + _elapsed / 2;
@@ -100,7 +108,7 @@ uint16_t calc_load(configRUN_TIME_COUNTER_TYPE _busy, configRUN_TIME_COUNTER_TYP
   return (uint16_t)load;
 }
 
-configRUN_TIME_COUNTER_TYPE tasks_stats(configRUN_TIME_COUNTER_TYPE _elapsed, uint16_t *_p_load_acc)
+static configRUN_TIME_COUNTER_TYPE tasks_stats(configRUN_TIME_COUNTER_TYPE _elapsed, uint16_t *_p_load_acc)
 {
   if (_elapsed == 0)
     return 0;
@@ -117,7 +125,7 @@ configRUN_TIME_COUNTER_TYPE tasks_stats(configRUN_TIME_COUNTER_TYPE _elapsed, ui
     // TODO add sort by xTaskNumber:
     uint16_t load = calc_load(task->ulRunTimeCounter, _elapsed);
 
-    smon_printf("%s\t%u\t%2"PRIu16".%"PREFIX_FRACT""PRIu16"%%\t %u\t:%s",
+    print("%s\t%u\t%2"PRIu16".%"PREFIX_FRACT""PRIu16"%%\t %u\t:%s",
                 task->pcTaskName, task->usStackHighWaterMark,
                 load / SCALE, load % SCALE,
                 task->uxCurrentPriority, s_TaskState[task->eCurrentState]);
@@ -134,7 +142,7 @@ configRUN_TIME_COUNTER_TYPE tasks_stats(configRUN_TIME_COUNTER_TYPE _elapsed, ui
   return run_time;
 }
 
-void Thread(void *pvParameters)
+static void Thread(void *pvParameters)
 {
   TickType_t xLastWakeTime;
   xLastWakeTime = xTaskGetTickCount();
@@ -150,57 +158,52 @@ void Thread(void *pvParameters)
 
     // Clear screen:
     // smon_printf(CLEARSCR);
-    smon_printf(CLEAREOS GOTOYX, 0, 0);
+    print(CLEAREOS GOTOYX, 0, 0);
 
     // Header:
-    smon_printf(BOLD"TASK\tSTACK\tLOAD\tPrior.\tState"NORMAL);
-    smon_printf("----------------------------------------");
+    print(BOLD"TASK\tSTACK\tLOAD\tPrior.\tState"NORMAL);
+    print("----------------------------------------");
 
     uint16_t load_acc;
     configRUN_TIME_COUNTER_TYPE run_time;
     run_time = tasks_stats(elapsed, &load_acc);
 
     // Separator:
-    smon_printf("========================================");
-    // smon_printf("");
+    print("========================================");
 
     // CPU load:
     uint16_t load = calc_load(run_time, elapsed);
-    smon_printf(BOLD"CPU load:\t%"PRIu16".%"PREFIX_FRACT"u%%"NORMAL,
+    print(BOLD"CPU load:\t%"PRIu16".%"PREFIX_FRACT"u%%"NORMAL,
                 load / SCALE, load % SCALE);
 
     // DEBUG:
     #if SYS_MON_VIEW_DEBUG_INFO
     {
-      smon_printf("ACC load:\t%"PRIu16".%"PREFIX_FRACT""PRIu16"%%",
+      print("ACC load:\t%"PRIu16".%"PREFIX_FRACT""PRIu16"%%",
                   load_acc / SCALE, load_acc % SCALE);
 
       int16_t load_err = load - load_acc;
       bool sign = load_err < 0;
       if (sign) load_err = -load_err;
-      smon_printf("Err load:\t%s%"PRIu16".%"PREFIX_FRACT""PRIu16"%%", sign ? "-" : "",
+      print("Err load:\t%s%"PRIu16".%"PREFIX_FRACT""PRIu16"%%", sign ? "-" : "",
                   load_err / SCALE, load_err % SCALE);
 
-      smon_printf("Elapsed: \t%"PRI_FRACT" cnt", elapsed);
+      print("Elapsed: \t%"PRI_FRACT" cnt", elapsed);
     }
     #endif /* SYS_MON_VIEW_DEBUG_INFO */
 
     #if configSUPPORT_DYNAMIC_ALLOCATION
     {
-      smon_printf("FREE HEAP:\t%u\r\n", xPortGetFreeHeapSize());
-      smon_printf("MIN HEAP:\t%u\r\n", xPortGetMinimumEverFreeHeapSize());
+      print("FREE HEAP:\t%u\r\n", xPortGetFreeHeapSize());
+      print("MIN HEAP:\t%u\r\n", xPortGetMinimumEverFreeHeapSize());
     }
     #endif /* configSUPPORT_DYNAMIC_ALLOCATION */
+
+    print(CLEAREOS);
 
     // Delay:
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(SYS_MONITOR_UPDATE_PERIOD_MS));
   }
-}
-
-void rtmon_xmitCmpltCallback(void)
-{
-  BaseType_t res = xSemaphoreGive(s_SemXmitHandle);
-  assert(res == pdTRUE);
 }
 
 void smonitor_Init(void)
