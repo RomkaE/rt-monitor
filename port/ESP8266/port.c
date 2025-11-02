fd58cd357f1c2a5ba9e05a3413c64a615690680e
Roman Egoshin <egoshin.roman@gmail.com>
2025-11-02 18:06:51 +0100

    Fix EP8266 port

--- port/ESP8266/port.c
+++ port/ESP8266/port.c
@@ -1,13 +1,13 @@
 
 #include <assert.h>
-#include "sys_monitor_cfg.h"
-#include "../inc/port.h"
+#include "rtmon_config.h"
+#include "../port.h"
 
 // ESP8266 sdk:
 #include "sdkconfig.h"
 #include "driver/uart.h"
 
-void portSysMonitor_Init(void)
+void rtmon_portInit(void)
 {
   uart_config_t uart_config = {
       .baud_rate = 115200,
@@ -19,16 +19,16 @@ void portSysMonitor_Init(void)
   };
 
   esp_err_t res;
-  res = uart_param_config(SYS_MONITOR_UART_NUM, &uart_config);
+  res = uart_param_config(RTMON_CFG_UART_NUM, &uart_config);
   assert(res == ESP_OK);
-  res = uart_driver_install(SYS_MONITOR_UART_NUM, 0, 256, 0, NULL, 0);
+  res = uart_driver_install(RTMON_CFG_UART_NUM, 0, 256, 0, NULL, 0);
   assert(res == ESP_OK);
 }
 
-void portSysMonitor_TxBuff(const void *_buff, uint16_t _lenght)
+void rtmon_xmitBuf(const char *_buf, const size_t _lenght)
 {
-  int tx_size = uart_write_bytes(SYS_MONITOR_UART_NUM, _buff, _lenght);
+  int tx_size = uart_write_bytes(RTMON_CFG_UART_NUM, _buf, _lenght);
   assert(tx_size == _lenght);
-  esp_err_t res = uart_wait_tx_done(SYS_MONITOR_UART_NUM, portMAX_DELAY);
+  esp_err_t res = uart_wait_tx_done(RTMON_CFG_UART_NUM, portMAX_DELAY);
   assert(res == ESP_OK);
 }
