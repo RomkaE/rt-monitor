a63a968464d375e49a554fb952c80a4b89437faf
Roman Egoshin <egoshin.roman@gmail.com>
2026-07-14 16:19:09 +0200

    Fixed the SEGGER RTT port. Added support for terminals.

--- port/SEGGER_RTT/port.c
+++ port/SEGGER_RTT/port.c
@@ -1,11 +1,12 @@
 
 #include <stdbool.h>
+#include <stdint.h>
 #include "rtmon_config.h"
 #include "../port.h"
+
 #include "app-error/app_assert.h"
 #include "tusb.h"
 
-//#if defined(RTMON_TUD_CDC_IF)
 #if defined(RTMON_RTT_BUFF_IDX)
 
 #include "SEGGER_RTT.h"
@@ -13,46 +14,45 @@
 static osal_semaphore_def_t sem_def;
 static osal_semaphore_t sem_cdc;
 
-static char s_Buf[1024];
+static const uint8_t s_TermSwitch[2] = { 0xFFu, (uint8_t)('0' + RTMON_RTT_TERMINAL) };
 
 void rtmon_portInit(void)
 {
   sem_cdc = osal_semaphore_create(&sem_def);
   ASSERT(sem_cdc != NULL);
 
-  SEGGER_RTT_ConfigUpBuffer(RTMON_RTT_BUFF_IDX, "RTMON", s_Buf, sizeof(s_Buf), SEGGER_RTT_MODE_NO_BLOCK_SKIP);
+  SEGGER_RTT_Init();
 }
 
 void rtmon_xmitBuf(const char *_buf, const size_t _lenght)
 {
-//  if (tud_cdc_n_connected(RTMON_TUD_CDC_IF) == false)
-//    return;
-
   size_t buf_size = _lenght;
   do
   {
-    uint16_t cdc_size = (uint16_t)SEGGER_RTT_GetAvailWriteSpace(RTMON_RTT_BUFF_IDX);
-    if (cdc_size == 0)
+    uint32_t sent_n = 0;
+
+    SEGGER_RTT_LOCK();
     {
+      unsigned avail = SEGGER_RTT_GetAvailWriteSpace(RTMON_RTT_BUFF_IDX);
+      if (avail > sizeof(s_TermSwitch))
+      {
+        uint32_t send_n = TU_MIN((uint32_t)(avail - sizeof(s_TermSwitch)), (uint32_t)buf_size);
+        SEGGER_RTT_WriteNoLock(RTMON_RTT_BUFF_IDX, s_TermSwitch, sizeof(s_TermSwitch));
+        sent_n = SEGGER_RTT_WriteNoLock(RTMON_RTT_BUFF_IDX, _buf, send_n);
+      }
+    }
+    SEGGER_RTT_UNLOCK();
+
+    if (sent_n == 0)
+    {
+      // Channel 0 is full. Back off and retry:
       osal_task_delay(100);
       continue;
     }
 
-    // Write data to CDC:
-    uint32_t send_n = TU_MIN(cdc_size, buf_size);
-
-    uint32_t sent_n = SEGGER_RTT_WriteNoLock(RTMON_RTT_BUFF_IDX, _buf, send_n);
-    ASSERT(send_n == sent_n);
-
-    // Wait:
-//    if (!osal_semaphore_wait(sem_cdc, 1000))
-    {
-      // TODO - add logs?!
-    }
-
     // Iteration:
-    _buf += send_n;
-    buf_size -= send_n;
+    _buf += sent_n;
+    buf_size -= sent_n;
   } while(buf_size);
 }
 
